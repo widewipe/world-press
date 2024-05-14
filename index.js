f63f9cac10d382cc555284aa38eb2d42b86f6e4d
Amartya Chowdhury <132732400+widewipe@users.noreply.github.com>
2024-05-14 19:53:43 +0530

    feat--improve routing

--- index.js
+++ index.js
@@ -16,77 +16,17 @@ app.get("/", async (req, res) => {
   res.render("pages/home", { data: data });
 });
 
-app.get("/world", async (req, res) => {
+app.get("/category/:cat", async (req, res) => {
+  const category = req.params.cat;
   const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=world&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
+    `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07`
   );
   const data = await result.json();
 
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
+  res.render("pages/categ", { data: data, cat: category });
+})
 
-app.get("/nation", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=nation&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
 
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
-
-app.get("/business", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=business&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
-
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
-
-app.get("/tech", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=tech&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
-
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
-
-app.get("/entertainment", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=entertainment&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
-
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
-
-app.get("/sports", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=sports&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
-
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
-
-app.get("/science", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=science&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
-
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
-
-app.get("/health", async (req, res) => {
-  const result = await fetch(
-    "https://gnews.io/api/v4/top-headlines?category=health&lang=en&country=in&max=10&apikey=8484e5c5636400235a62f9f928ef1a07"
-  );
-  const data = await result.json();
-
-  res.render("pages/categ", { data: data, cat: req.route.path });
-});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
